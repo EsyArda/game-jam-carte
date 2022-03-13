0875a9c0c21475d386fc4cd692def058c4d8e344
Thomas QUENTEL <thomas.quentel@etu.uca.fr>
2022-03-13 10:14:34 +0100

    Transition entre les scènes fonctionnelle

--- LordOfTheMap/Assets/MenuScript.cs
+++ LordOfTheMap/Assets/MenuScript.cs
@@ -5,14 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    void Start()
-    {
-        if (SceneManager.GetActiveScene().name != "menu")
-        {
-            SceneManager.LoadScene("menu");
-        }
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) //(SceneManager.GetActiveScene().name != "Lotr")
