d56a9cb7ca8c51b5833d35e64a3807824f16cd89
MarianPoirot <81marianp@gmail.com>
2022-03-13 12:39:28 +0100

    Map principale (appui sur tab)
    MiniMap désactivée (aisément réactivable si besoin)

---
+++ LordOfTheMap/Assets/Creator Kit - RPG/Scripts/UI/MapCameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapCameraController : MonoBehaviour
+{
+    public int speed = 3;
+    private Vector3 targetGo;
+    private Vector3 targetGoBack;
+
+    void Start()
+    {
+        float step = speed;
+        targetGo = new Vector3(960, 540,0);
+        targetGoBack = new Vector3(960, -500,0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetGo, speed);
+            GameObject.Find("Map Camera").GetComponent<Camera>().enabled = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetGoBack, speed);
+            GameObject.Find("Map Camera").GetComponent<Camera>().enabled = true;
+        }
+    }
+}
