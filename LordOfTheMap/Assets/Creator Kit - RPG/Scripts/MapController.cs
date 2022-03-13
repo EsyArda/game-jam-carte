982096ed77ec16139348cc0425d80e5c861caf5e
MarianPoirot <81marianp@gmail.com>
2022-03-13 13:32:09 +0100

    Affichage de la map

---
+++ LordOfTheMap/Assets/Creator Kit - RPG/Scripts/MapController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour
+{
+    public int sortingOrder = 0;
+    private SpriteRenderer sprite;
+    public int sortingOrder2 = 1000;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (sprite)
+                sprite.sortingOrder = sortingOrder;
+        }
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            if (sprite)
+                sprite.sortingOrder = sortingOrder2;
+        }
+    }
+}
